d47fb7a2f0223f849da12bd6e689643132faecb0
root <root@localhost.localdomain>
2020-01-04 16:49:41 +0900

    edited home contents

--- frontend/src/components/HomeContents.vue
+++ frontend/src/components/HomeContents.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="Home">
     <v-card align="center" justify="center">
-      <v-img id="homeImg" :src="HomeImage" height="1000">
+      <v-img
+        id="homeImg"
+        :src="HomeImage"
+        height="700"
+        gradient="to top right, rgba(255,255,255,.1),rgba(255,255,255,.9)"
+      >
         <div class="homeInfo">
-          <v-card-title class="display-3 font-weight-black"
-            >회사 이름</v-card-title
-          >
+          <v-card-title class="display-3 font-weight-black">회사 이름</v-card-title>
           <v-card-title class="display-1">
             제품보러가기
             <v-icon>fas fa-arrow-right</v-icon>
@@ -13,6 +16,18 @@
         </div>
       </v-img>
     </v-card>
+
+    <v-container fluid>
+      <v-row dense>
+        <v-col v-for="card in cards" :key="card.id" cols="3">
+          <v-card background-color="transparent">
+            <v-icon justify="center">{{ card.image }}</v-icon>
+            <v-card-text>{{ card.text }}</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+
     <v-parallax src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
       <v-row align="center" justify="center">
         <h1>준비중</h1>
@@ -24,7 +39,29 @@
 <script>
 export default {
   data: () => ({
-    HomeImage: require('../../public/img/fabric2.jpg'),
+    HomeImage: require('../../public/img/leatherFabric4.jpg'),
+    cards: [
+      {
+        id: 0,
+        image: 'fas fa-cogs fa-5x',
+        text: 'djeifsl jejfsi  ejfiowf knvozknf ijf,xve',
+      },
+      {
+        id: 1,
+        image: 'fab fa-pagelines fa-5x',
+        text: 'foijaa woi wifns ijw qinfklsfiw  jfiwof',
+      },
+      {
+        id: 2,
+        image: 'fas fa-users fa-5x',
+        text: 'wiej fjsoivkcownkxo jiwo ofiwn ',
+      },
+      {
+        id: 3,
+        image: 'far fa-lightblub fa-5x',
+        text: 'sie jsie ejo esjiof ejiofs',
+      },
+    ],
   }),
 };
 </script>
