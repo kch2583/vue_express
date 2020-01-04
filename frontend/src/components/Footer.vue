d9df644bc891a9a847ea4d5aeedaacb14363dbd2
root <root@localhost.localdomain>
2020-01-04 20:52:11 +0900

    created meta

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,34 +1,34 @@
 <template>
-  <v-footer padless >
+  <v-footer padless>
     <v-card width="100%" flat tile class="indigo litghten-1 text-center">
       <v-card-text>
         <v-btn v-for="icon in icons" :key="icon" class="mx-4 white--text" icon>
           <v-icon size="24px">{{ icon }}</v-icon>
         </v-btn>
       </v-card-text>
-      <v-card-text class="white--text pt-0">
-        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      <v-card-text class="white--text">
+        회사명 : 주식회사 대현
+        <br />사업자번호 : 203-129-245463
+        <br />주소 : 경기도 포천시 가산면 너배기길 90
+        <br />대표자 : 김땡땡
       </v-card-text>
-
       <v-divider></v-divider>
-      <v-card-text>&copy; {{ new Date().getFullYear() }} </v-card-text>
+      <v-card-text>Copyright 2020. 주식회사 대현 Corp. All Right Reserved.</v-card-text>
     </v-card>
-    
   </v-footer>
 </template>
 <script>
 export default {
   data: () => ({
     icons: [
-       'fab fa-facebook',
-        'fab fa-twitter',
-        'fab fa-google-plus',
-        'fab fa-linkedin',
-        'fab fa-instagram',
-    ]
-  })
-}
+      'fab fa-facebook',
+      'fab fa-twitter',
+      'fab fa-google-plus',
+      'fab fa-linkedin',
+      'fab fa-instagram',
+    ],
+  }),
+};
 </script>
 <style lang="scss">
-  
 </style>
